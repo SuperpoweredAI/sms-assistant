ffed38a6da9b68edfe80d635a898e5ce99213f73
justin <justinmauriceclark@gmail.com>
2023-03-05 18:47:25 -0700

    add a deploy script that devs can run to build/deploy the cloudformation stack

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+set -e
+
+sam build -t resources.yaml
+
+sam deploy \
+    --stack-name sms-assistant-demo-1 \
+    --region us-east-1 \
+    --no-confirm-changeset \
+    --resolve-s3 \
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides ParameterKey=Environment,ParameterValue=dev
