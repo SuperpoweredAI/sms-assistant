a7d63bad77683acec3b852cb378626a0c6e530bd
Justin Clark <justinmauriceclark@gmail.com>
2023-03-12 23:12:02 -0600

    add TwilioPhoneNumber to deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,7 @@ sam deploy \
     --no-confirm-changeset \
     --resolve-s3 \
     --capabilities CAPABILITY_IAM \
-    --parameter-overrides ParameterKey=Environment,ParameterValue=dev ParameterKey=SuperpoweredModelId,ParameterValue=c8a058d3-84a2-4a4a-af98-75259c42e52e
+    --parameter-overrides \
+        ParameterKey=Environment,ParameterValue=dev \
+        ParameterKey=SuperpoweredModelId,ParameterValue=c8a058d3-84a2-4a4a-af98-75259c42e52e \
+        ParameterKey=TwilioPhoneNumber,ParameterValue=+18446037222
